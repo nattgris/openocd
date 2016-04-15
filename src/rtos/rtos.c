da03875be803da4140cbe7e9226e06a093ef7e81
Andreas Fritiofson <andreas.fritiofson@gmail.com>
2016-04-15 12:38:43 +0200

    rtos: Support rt-kernel
    Works for the proprietary rt-kernel from rt-labs.
    
    Requires a patch to expose certain debug symbols, see
    contrib/rtos-helpers/rt-kernel.patch
    
    Change-Id: Id2c2e292c15fb17eab25e3d07db05014daa2a2b0
    Signed-off-by: Andreas Fritiofson <andreas.fritiofson@gmail.com>

--- src/rtos/rtos.c
+++ src/rtos/rtos.c
@@ -37,6 +37,7 @@ extern struct rtos_type embKernel_rtos;
 extern struct rtos_type mqx_rtos;
 extern struct rtos_type uCOS_III_rtos;
 extern struct rtos_type nuttx_rtos;
+extern struct rtos_type rtkernel_rtos;
 extern struct rtos_type hwthread_rtos;
 extern struct rtos_type riot_rtos;
 
@@ -52,6 +53,7 @@ static struct rtos_type *rtos_types[] = {
 	&uCOS_III_rtos,
 	&nuttx_rtos,
 	&riot_rtos,
+	&rtkernel_rtos,
 	/* keep this as last, as it always matches with rtos auto */
 	&hwthread_rtos,
 	NULL
